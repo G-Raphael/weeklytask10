93f72944240868718c358986ff04480fa29acc6d
G-Raphael <emefekeraphael@gmail.com>
2021-08-07 11:42:57 +0100

    updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="home">
           <h1 className="homeintro">Front end web<br/> Development</h1>
-          <button className="homebtn"><span></span>More<span></span><img src=""></img></button>
+          <button className="homebtn">More</button>
           <h1 className="homecontent">This is the home page</h1>
 
         </div>
